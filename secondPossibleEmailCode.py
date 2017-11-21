123a97d27c5bf5babe8664ba492f0a32bbf39ba9
tbd1998 <32682011+tbd1998@users.noreply.github.com>
2017-11-21 12:20:22 -0600

    secondEmailPossibility

---
+++ secondPossibleEmailCode.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+s = smtplib.SMTP(host = 'smtp.gmail.com', port = 587)
+s.starttls();
+s.login("weatherstationdonotreply@gmail.com", "weatherstation");
+
+msg = MIMEMultipart()
+
+message = "this is a test message"
+
+msg['From'] = "weatherstationdonotreply@gmail.com"
+msg['To'] = "delias3@hawk.iit.edu";
+msg['Subject'] = "weather"
+
+msg.attach(MIMEText(message, 'plain'))
+
+s.send_message(msg)
+
+s.quit()
